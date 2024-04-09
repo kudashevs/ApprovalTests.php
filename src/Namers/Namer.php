1f8b5d649fb8228f162b14ce5ed09c4c67016fc2
Sergey Kudashev <kudashevs@gmail.com>
2024-04-09 14:32:42 +0300

    Style Namer update code formatting according to the PSR style guide

--- src/Namers/Namer.php
+++ src/Namers/Namer.php
@@ -3,9 +3,14 @@
 interface Namer
 {
     public function getApprovedFile($extensionWithoutDot);
+
     public function getReceivedFile($extensionWithoutDot);
+
     public function getCallingTestClassName();
+
     public function getCallingTestClassNameWithoutNamespace();
+
     public function getCallingTestMethodName();
+
     public function getCallingTestDirectory();
 }
