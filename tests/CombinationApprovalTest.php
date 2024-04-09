ae04f96cee2eaa0b44f2167d199810dacc53a937
Sergey Kudashev <kudashevs@gmail.com>
2024-04-09 14:33:27 +0300

    Style CombinationApprovalTest fix code formatting

--- tests/CombinationApprovalTest.php
+++ tests/CombinationApprovalTest.php
@@ -4,17 +4,20 @@ use ApprovalTests\CombinationApprovals;
 use ApprovalTests\SkipCombinationException;
 use PHPUnit\Framework\TestCase;
 
-class CombinationApprovalTest extends TestCase {
-    public function testStep1() {
+class CombinationApprovalTest extends TestCase
+{
+    public function testStep1()
+    {
         $values = [1, 2, 3];
-        CombinationApprovals::verifyAllCombinations1(function($a1) {
+        CombinationApprovals::verifyAllCombinations1(function ($a1) {
             return implode(", ", [$a1]);
         }, $values);
     }
 
-    public function testStepSkipping() {
+    public function testStepSkipping()
+    {
         $values = [1, 2, 3];
-        CombinationApprovals::verifyAllCombinations2(function($a, $b) {
+        CombinationApprovals::verifyAllCombinations2(function ($a, $b) {
             if ($a * $b === 2) {
                 throw new Exception("No twos");
             }
@@ -25,9 +28,10 @@ class CombinationApprovalTest extends TestCase {
         }, $values, $values);
     }
 
-    public function testStep9() {
+    public function testStep9()
+    {
         $values = [1, 2, 3];
-        CombinationApprovals::verifyAllCombinations9(function($a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9) {
+        CombinationApprovals::verifyAllCombinations9(function ($a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9) {
             return implode(", ", [$a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8, $a9]);
         }, $values, $values, $values, $values, $values, $values, $values, $values, $values);
     }
