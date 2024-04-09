2968a91bbd2b3f204724ab057313c2540dd8d530
Sergey Kudashev <kudashevs@gmail.com>
2024-04-09 14:26:02 +0300

    Style SortingUtil update code formatting according to the PSR style guide

--- src/SortingUtil.php
+++ src/SortingUtil.php
@@ -5,7 +5,10 @@ class SortingUtil
     // https://gist.github.com/cdzombak/601849
     private static function ksortRecursive(&$array, $sort_flags = SORT_REGULAR)
     {
-        if (!is_array($array)) return false;
+        if (!is_array($array)) {
+            return false;
+        }
+
         ksort($array, $sort_flags);
         foreach ($array as &$arr) {
             self::ksortRecursive($arr, $sort_flags);
